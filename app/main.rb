3b8c0abfe64b3b6f9f766fecc7be47ba72e72358
Ramses <johnlightz@gmail.com>
2015-01-27 22:26:29 +0000

    Had some fun adding a very simple about extension page
    It basically does the same thing as the home page

--- app/main.rb
+++ app/main.rb
@@ -4,6 +4,11 @@ require 'sequel'
 
 Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://marketshipdev.db')
 
+get '/about/:input' do
+        @param1= params[:input]
+        slim :about
+end
+
 get '/:input' do
 	@param1 = params[:input]
 	slim :index
